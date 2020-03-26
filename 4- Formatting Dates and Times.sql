2e273b9c9e1f28bb16a7ece2c5d5bc7ac0ca2814
franktranvantu <franktranvantu@gmail.com>
2020-03-26 23:06:07 +0700

    Add date time formatting

---
+++ 4- Formatting Dates and Times.sql
@@ -0,0 +1,5 @@
+SELECT DATE_FORMAT(NOW(), '%m/%d/%y'), DATE_FORMAT(NOW(), '%M %d %Y');
+
+SELECT TIME_FORMAT(NOW(), '%H:%i:%s'), TIME_FORMAT(NOW(), '%h:%i:%s %p');
+
+Search google: mysql date format string

20f40d0f42a08d9cfc28135a5e720339d034aaac
franktranvantu <franktranvantu@gmail.com>
2020-04-17 22:54:37 +0700

    Add trigger

--- 10-Triggers and Events/1-trigger.sql
+++ 10-Triggers and Events/1-trigger.sql
@@ -7,6 +7,19 @@ FOR EACH ROW
 BEGIN
 	UPDATE invoices
     SET payment_total = payment_total + NEW.amount
-    WHERE client_id = NEW.client_id;
+    WHERE invoice_id = NEW.invoice_id;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS payments_after_delete;
+
+DELIMITER $$
+CREATE TRIGGER payments_after_delete
+AFTER DELETE ON payments
+FOR EACH ROW
+BEGIN
+	UPDATE invoices
+    SET payment_total = payment_total - OLD.amount
+    WHERE invoice_id = OLD.invoice_id;
 END$$
 DELIMITER ;

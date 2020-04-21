232f677743a0d04b6ba359aed5bdd39ca62b52e6
franktranvantu <franktranvantu@gmail.com>
2020-04-22 00:45:32 +0700

    Add fixed point and floating point

---
+++ 12-Data types/4-fixed-point-and-floating-point.sql
@@ -0,0 +1,10 @@
+-- For currency numbers
+DECIMAL(p, s) -- precision: [1, 65], scale
+-- DECIMAL(9, 2) => 1234567.89
+DEC
+NUMERIC 
+FIXED 
+
+-- For very large/small numbers
+FLOAT       -- 4 bytes
+DOUBLE      -- 8 bytes

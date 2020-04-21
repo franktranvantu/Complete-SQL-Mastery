5cfc8ac93b50ffdff822032e802ece9e7a71df0f
franktranvantu <franktranvantu@gmail.com>
2020-04-22 00:36:16 +0700

    Add integer types

---
+++ 12-Data types/3-integer-types.sql
@@ -0,0 +1,8 @@
+TINYINT             -- 1 byte [-128, 127]
+UNSIGNED TINYINT    -- 1 byte [0, 255]
+SMALLINT            -- 2 bytes [-32K, 32K]
+MEDIUMINT           -- 3 bytes [-8M, 8M]
+INT                 -- 4 bytes [-2B, 2B]
+BIGINT              -- 8 bytes [-9Z, 9Z]
+
+-- Google: mysql integer types
